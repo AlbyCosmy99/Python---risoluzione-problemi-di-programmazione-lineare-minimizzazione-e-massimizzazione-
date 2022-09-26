4109ec50dda4fff77a46eacba2eaa45007550832
Andrei <72943982+AlbyCosmy99@users.noreply.github.com>
2022-09-26 15:06:08 +0200

    Create pyomo.py

---
+++ pyomo.py
@@ -0,0 +1,25 @@
+import pyomo.environ as pe
+#from pyomo.opt import SolverFactory
+
+def buildmodel():
+    #model
+    model = pe.ConcreteModel()
+    #variables
+    model.x = pe.Var(domain=pe.NonNegativeReals)
+    model.y = pe.Var(domain=pe.NonNegativeReals)
+    #objective
+    model.obj = pe.Objective(expr = 40*model.x + 50*model.y, sense = pe.maximize)
+    #constraints
+    model.c1 = pe.Constraint(expr = model.x + model.y <= 50)
+    model.c2 = pe.Constraint(expr = 3*model.x + 4*model.y <=180)
+    model.c3 = pe.Constraint(expr = model.y <=40)
+    return model
+#def main():
+    
+    
+if __name__ == "__main__":
+    model = buildmodel()
+    opt = pe.SolverFactory("glpk")
+    opt.solve(model)
+    print("x: ",pe.value(model.x))
+    print("y: ",pe.value(model.y))
